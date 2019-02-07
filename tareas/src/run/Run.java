67b99cb0e666692fcc10e9d6af7a2c05b48df0ee
Lenovo <Lenovo@DESKTOP-0KR6K0S>
2019-02-06 19:54:43 -0500

    inicio del proyecto falta modelo y percistence

---
+++ tareas/src/run/Run.java
@@ -0,0 +1,12 @@
+package run;
+import utilities.utilites;
+public class Run {
+
+	public static void main(String[] args) {
+	utilites ut=new utilites();
+	System.out.println(ut.encripted("men saque 1 en el parcial"));
+	System.out.println(ut.encripted("p le ne 1 euqas nemlaicra"));
+	
+	}
+
+}
